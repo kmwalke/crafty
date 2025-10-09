76fb17e42683dd0d2c55cb9b8121ae7a61644bee
Kent <kentslaymaker@gmail.com>
2025-10-09 15:18:11 -0600

    init travel spec

--- spec/features/traveling_spec.rb
+++ spec/features/traveling_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Game' do
+RSpec.describe 'Traveling' do
   describe 'logged out' do
     it 'redirects to login page' do
       visit game_path
@@ -14,23 +14,19 @@ RSpec.describe 'Game' do
       visit game_path
     end
 
-    it 'displays the homepage' do
-      expect(page).to have_content('Crafty')
-    end
-
-    it 'displays location' do
+    it 'shows location details' do
+      # either show all location details or click for a popup div
       expect(true).to be false
     end
 
-    it 'displays status' do
+    it 'travels between locations' do
+      # travel to a new location
       expect(true).to be false
     end
 
-    it 'displays inventory' do
-      expect(true).to be false
-    end
-
-    it 'displays actions' do
+    it 'shows the map' do
+      # click "map" to see a list of locations
+      # In the future, an actual map, using the x,y coors and a background satellite image
       expect(true).to be false
     end
   end
