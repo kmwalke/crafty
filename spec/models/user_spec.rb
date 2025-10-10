3ed3bad50992fbe2d2510c31b1610b62202e538a
Kent <kentslaymaker@gmail.com>
2025-10-10 10:03:18 -0600

    distracted by building out trading specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,6 +35,75 @@ RSpec.describe User, type: :model do
   describe 'actions' do
     let(:user) { create(:user) }
 
+    describe 'inventory' do
+      let(:item) { create(:item) }
+
+      it 'picks up items' do
+        user.add_to_inventory(item)
+
+        expect(user.inventory.include? item).to be_true
+      end
+
+      it 'cannot remove item from other user' do
+        user2 = create(:user)
+
+        user2.add_to_inventory(item)
+        user.add_to_inventory(item)
+
+        expect(user.inventory.include? item).to be_false
+      end
+
+      describe 'trading', skip: 'not built. this file is gonna be big.  SPlit into another file?  Make userspec a folder?' do
+        it 'gifts item to another user' do
+          before :each do
+            user2 = create(:user)
+
+            user2.add_to_inventory(item)
+
+            user2.trade(
+              user: user,
+              export: [item],
+              import: nil
+            )
+          end
+
+          it 'adds to giftee inventory' do
+            expect(user.inventory.include?(item)).to be_true
+          end
+
+          it 'removes from gifter inventory' do
+            expect(user2.inventory.include?(item)).to be_false
+          end
+        end
+
+        it 'trades with another user' do
+          before :each do
+            user2 = create(:user)
+            item2 = create(:item)
+            item3 = create(:item)
+
+            user.add_to_inventory(item)
+            user2.add_to_inventory(item2)
+            user2.add_to_inventory(item3)
+
+            user.trade(
+              user: user2,
+              export: [item],
+              import: [item2, item3]
+            )
+          end
+
+          it 'adds items to user' do
+            expect(Set[item2, item3].subset? user.inventory.to_set).to be_true
+          end
+
+          it 'removes item from user' do
+            expect(user2.inventory.include? item).to be_true
+          end
+        end
+      end
+    end
+
     it 'cannot equip a vehicle not in inventory' do
       vehicle = create(:vehicle)
 
