88ad1e530ec329630e7237c5579ec513cbdd0fb0
Kent <kentslaymaker@gmail.com>
2025-10-12 06:45:16 -0600

    init game page

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,7 @@
 class GameController < ApplicationController
   before_action :logged_in
 
-  def index; end
+  def index
+    @local_players = User.where(location: @current_user.location)
+  end
 end
