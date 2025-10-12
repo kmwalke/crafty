0060930b5c154b2b0265806d304c58c73a7ca423
Kent <kentslaymaker@gmail.com>
2025-10-12 14:15:57 -0600

    travel spec

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle < Item
   has_one :user
 
   def actions
-    %w[travel]
+    %w[Travel]
   end
 
   def travel(location)
--- spec/features/traveling_spec.rb
+++ spec/features/traveling_spec.rb
@@ -2,25 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Traveling' do
   describe 'logged in' do
+    let!(:current_user) { login }
+    let!(:location1) { current_user.location }
+    let!(:location2) { create(:location) }
+    let!(:location3) { create(:location) }
+
     before do
-      login
+      current_user.equip_vehicle(create(:vehicle, inventory: current_user.inventory))
       visit game_path
     end
 
-    it 'shows location details' do
-      # either show all location details or click for a popup div
-      expect(true).to be false
-    end
-
     it 'travels between locations' do
-      # travel to a new location
-      expect(true).to be false
+      click_link 'Travel'
+      click_link location2.name
+
+      expect(current_user.reload.location).to eq(location2)
     end
 
-    it 'shows the map' do
-      # click "map" to see a list of locations
+    describe 'shows the map' do
       # In the future, an actual map, using the x,y coors and a background satellite image
-      expect(true).to be false
+      let!(:locations) { [location1, location2, location3] }
+
+      it 'shows valid travel locations' do
+        locations.each do |location|
+          expect(page).to have_content(location.name)
+        end
+      end
+
+      pending 'doesnt show distant locations'
     end
   end
 end
