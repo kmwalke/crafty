49e6b893f3679d120bba34d92af77b8f83ac0900
Kent <kentslaymaker@gmail.com>
2025-10-13 15:17:45 -0600

    gather spec

--- spec/features/gathering_spec.rb
+++ spec/features/gathering_spec.rb
@@ -9,5 +9,27 @@ RSpec.describe 'Gathering' do
   end
 
   it 'displays the location\'s resources' do
+    expect(true).to be false
+  end
+
+  it 'gathers resources' do
+    expect(true).to be false
+  #   Multiple types of tools
+  # Axe gathers lumber from trees
+  # sheers gather wool
+  # pickaxe gathers ores
+  # secateurs gather herbs/fruits
+  # etc...
+  # various tools inherit from Tool class
+  # Use subtype, somehow.  Maybe polymorphism, maybe just a method that checks it
+  # add equipping tools to equipment spec
+  end
+
+  it 'adds gathered resources to inventory' do
+    expect(true).to be false
+  end
+
+  it 'checks the various types of tools' do
+    expect(true).to be false
   end
 end
