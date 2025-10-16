7f8960c7186c05102958033c5c398941bbb8fca6
Kent <kentslaymaker@gmail.com>
2025-10-16 13:39:11 -0600

    user_actions spec

--- spec/models/user_actions_spec.rb
+++ spec/models/user_actions_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User do
   let(:user) { create(:user) }
+  let(:vehicle) { create(:vehicle, inventory: user.inventory) }
+  let(:gathering_tool) { create(:gathering_tool, inventory: user.inventory) }
 
   describe 'actions' do
-    pending 'combine actions from multiple pieces of equipment'
+    it 'has no actions without equipment' do
+      expect(user.actions).to eq([])
+    end
+
+    it 'combines actions from multiple pieces of equipment' do
+      user.equip_item vehicle
+      user.equip_item gathering_tool
+
+      expect(user.actions).to eq(%w[gather travel])
+    end
   end
 end
