8a8e06f6060a4914e2b5f4253140a14948a99c32
Kent <kentslaymaker@gmail.com>
2025-10-17 09:12:07 -0600

    add stack_acmount to itme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_16_184402) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_17_150349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_16_184402) do
     t.datetime "updated_at", null: false
     t.string "subtype"
     t.string "color", null: false
+    t.integer "stack_amount", default: 1, null: false
   end
 
   create_table "levels", force: :cascade do |t|
