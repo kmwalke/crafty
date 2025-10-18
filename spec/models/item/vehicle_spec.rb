8963203feb926978b855bad2b0d991ff44666dde
Kent <kentslaymaker@gmail.com>
2025-10-18 08:19:25 -0600

    equipped items colors

--- spec/models/item/vehicle_spec.rb
+++ spec/models/item/vehicle_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Item::Vehicle do
       expect { create(:vehicle).travel(location2) }.to raise_error(CraftyError)
     end
 
-    pending 'the below spec sometimes fails with "can only spend positive energy".  In specs and in real life'
+    pending 'the below spec sometimes fails with "can only spend positive energy". In specs and in real life'
     it 'updates the users location' do
       vehicle.travel(location2)
 
