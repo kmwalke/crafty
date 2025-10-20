9cf81e4dd9a9db7792643d86bf1f972810c45743
Kent <kentslaymaker@gmail.com>
2025-10-20 13:37:13 -0600

    display buildings

--- app/utilities/item_util.rb
+++ app/utilities/item_util.rb
@@ -12,7 +12,7 @@ class ItemUtil
         description: 'Gathering place for new players. Contains sales listings.',
         level: Level::COMMON,
         created_by: User.first,
-        color: '#663300'
+        color: '#000099'
       ),
       Item::Craftable::Building.new(
         name: 'Traders Guild Hall',
