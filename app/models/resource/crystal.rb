bdf939399439fb7e2ff769dd4630d860319d8367
Kent <kentslaymaker@gmail.com>
2025-10-20 14:39:20 -0600

    resource display first draft

--- app/models/resource/crystal.rb
+++ app/models/resource/crystal.rb
@@ -1,7 +1,12 @@
 class Resource::Crystal < Resource
+  def default_color
+    '#ff0066'.freeze
+  end
+
   def gather
     Item::Gatherable::Shard.new(
       name:,
+      color:,
       description: 'A small bit of crystal.',
       level: item_level
     )
--- app/utilities/location_util.rb
+++ app/utilities/location_util.rb
@@ -2,117 +2,114 @@ class LocationUtil
   attr_accessor :location
 
   def self.provision_locations
-    location_data.each do |info|
-      LocationUtil.new.provision_location(name: info[:name], pos_x: info[:pos_x], pos_y: info[:pos_y],
-                                          description: info[:description], bg_color: info[:bg_color],
-                                          highlight_color: info[:highlight_color])
+    location_data.each do |location|
+      LocationUtil.new.provision_location(location)
     end
   end
 
-  def provision_location(name: nil, pos_x: nil, pos_y: nil, description: nil, bg_color: nil, highlight_color: nil)
-    @location = Location.find_or_create_by(name:, pos_x:, pos_y:, description:, bg_color:, highlight_color:)
+  def provision_location(location)
+    location.save
+    @location = location
     create_resources
   end
 
   def create_resources
-    LocationUtil.resource_data.each do |datum|
-      if datum[:location] == @location.name
-        Resource.create(type: datum[:type], name: datum[:name], location: @location, level: datum[:level])
-      end
+    LocationUtil.resource_data.each do |resource|
+      resource.save if resource.location_id == @location.id
     end
   end
 
   def self.resource_data
     [
-      { type: ResourceType::TYPES[:crystal], name: 'Singing', location: location_data[1][:name], level: Level::COMMON },
-      { type: ResourceType::TYPES[:fish], name: 'Babbling Brook', location: location_data[1][:name], level: Level::COMMON },
-      { type: ResourceType::TYPES[:forest], name: 'Sun Dappled', location: location_data[1][:name], level: Level::COMMON },
-      { type: ResourceType::TYPES[:bush], name: 'Blackberry', location: location_data[1][:name], level: Level::UNCOMMON },
-      { type: ResourceType::TYPES[:herd], name: 'Grazing Sheep', location: location_data[1][:name], level: Level::COMMON },
-      { type: ResourceType::TYPES[:meadow], name: 'Blooming', location: location_data[1][:name], level: Level::COMMON },
-      { type: ResourceType::TYPES[:ore], name: 'Copper', location: location_data[1][:name], level: Level::COMMON },
-      { type: ResourceType::TYPES[:ore], name: 'Iron', location: location_data[1][:name], level: Level::COMMON },
+      Resource::Crystal.build(name: 'Singing', location_id: 2, level: Level::COMMON),
+      Resource::Fish.build(name: 'Babbling Brook', location_id: 2, level: Level::COMMON),
+      Resource::Forest.build(name: 'Sun Dappled', location_id: 2, level: Level::COMMON),
+      Resource::Bush.build(name: 'Blackberry', location_id: 2, level: Level::UNCOMMON),
+      Resource::Herd.build(name: 'Grazing Sheep', location_id: 2, level: Level::COMMON),
+      Resource::Meadow.build(name: 'Blooming', location_id: 2, level: Level::COMMON),
+      Resource::Ore.build(name: 'Copper', location_id: 2, level: Level::COMMON),
+      Resource::Ore.build(name: 'Iron', location_id: 2, level: Level::COMMON),
 
-      { type: ResourceType::TYPES[:crystal], name: 'Cracked Quartz', location: location_data[2][:name], level: Level::RARE },
-      { type: ResourceType::TYPES[:herd], name: 'Skittering Lizard', location: location_data[2][:name], level: Level::UNCOMMON },
-      { type: ResourceType::TYPES[:meadow], name: 'Sparse Cacti', location: location_data[2][:name], level: Level::UNCOMMON },
-      { type: ResourceType::TYPES[:ore], name: 'Copper', location: location_data[2][:name], level: Level::UNCOMMON },
-      { type: ResourceType::TYPES[:ore], name: 'Iron', location: location_data[2][:name], level: Level::UNCOMMON },
-      { type: ResourceType::TYPES[:ore], name: 'Titanium', location: location_data[2][:name], level: Level::COMMON },
-      { type: ResourceType::TYPES[:ore], name: 'Aluminum', location: location_data[2][:name], level: Level::COMMON },
+      Resource::Crystal.build(name: 'Cracked Quartz', location_id: 3, level: Level::RARE),
+      Resource::Herd.build(name: 'Skittering Lizard', location_id: 3, level: Level::UNCOMMON),
+      Resource::Meadow.build(name: 'Sparse Cacti', location_id: 3, level: Level::UNCOMMON),
+      Resource::Ore.build(name: 'Copper', location_id: 3, level: Level::UNCOMMON),
+      Resource::Ore.build(name: 'Iron', location_id: 3, level: Level::UNCOMMON),
+      Resource::Ore.build(name: 'Titanium', location_id: 3, level: Level::COMMON),
+      Resource::Ore.build(name: 'Aluminum', location_id: 3, level: Level::COMMON),
 
-      { type: ResourceType::TYPES[:crystal], name: 'Roselite', location: location_data[3][:name], level: Level::RARE },
-      { type: ResourceType::TYPES[:fish], name: 'Fetid Pool', location: location_data[3][:name], level: Level::RARE },
-      { type: ResourceType::TYPES[:forest], name: 'Twisted', location: location_data[3][:name], level: Level::UNCOMMON },
-      { type: ResourceType::TYPES[:bush], name: 'Poison Apple', location: location_data[3][:name], level: Level::RARE },
-      { type: ResourceType::TYPES[:herd], name: 'Mangy Wolf', location: location_data[3][:name], level: Level::RARE },
-      { type: ResourceType::TYPES[:meadow], name: 'Briar Patch', location: location_data[3][:name], level: Level::COMMON },
+      Resource::Crystal.build(name: 'Roselite', location_id: 4, level: Level::RARE),
+      Resource::Fish.build(name: 'Fetid Pool', location_id: 4, level: Level::RARE),
+      Resource::Forest.build(name: 'Twisted', location_id: 4, level: Level::UNCOMMON),
+      Resource::Bush.build(name: 'Poison Apple', location_id: 4, level: Level::RARE),
+      Resource::Herd.build(name: 'Mangy Wolf', location_id: 4, level: Level::RARE),
+      Resource::Meadow.build(name: 'Briar Patch', location_id: 4, level: Level::COMMON),
 
-      { type: ResourceType::TYPES[:crystal], name: 'Obsidian', location: location_data[4][:name], level: Level::EPIC },
-      { type: ResourceType::TYPES[:bush], name: 'Habanero', location: location_data[4][:name], level: Level::RARE },
-      { type: ResourceType::TYPES[:herd], name: 'Lava Frog', location: location_data[4][:name], level: Level::EPIC },
-      { type: ResourceType::TYPES[:meadow], name: 'Red Cactus', location: location_data[4][:name], level: Level::EPIC },
-      { type: ResourceType::TYPES[:ore], name: 'Copper', location: location_data[4][:name], level: Level::EPIC },
-      { type: ResourceType::TYPES[:ore], name: 'Iron', location: location_data[4][:name], level: Level::EPIC },
-      { type: ResourceType::TYPES[:ore], name: 'Titanium', location: location_data[4][:name], level: Level::RARE },
-      { type: ResourceType::TYPES[:ore], name: 'Aluminum', location: location_data[4][:name], level: Level::RARE },
+      Resource::Crystal.build(name: 'Obsidian', location_id: 5, level: Level::EPIC),
+      Resource::Bush.build(name: 'Habanero', location_id: 5, level: Level::RARE),
+      Resource::Herd.build(name: 'Lava Frog', location_id: 5, level: Level::EPIC),
+      Resource::Meadow.build(name: 'Red Cactus', location_id: 5, level: Level::EPIC),
+      Resource::Ore.build(name: 'Copper', location_id: 5, level: Level::EPIC),
+      Resource::Ore.build(name: 'Iron', location_id: 5, level: Level::EPIC),
+      Resource::Ore.build(name: 'Titanium', location_id: 5, level: Level::RARE),
+      Resource::Ore.build(name: 'Aluminum', location_id: 5, level: Level::RARE),
 
-      { type: ResourceType::TYPES[:crystal], name: 'Singing', location: location_data[5][:name], level: Level::LEGENDARY },
-      { type: ResourceType::TYPES[:crystal], name: 'Human-like Bone', location: location_data[5][:name], level: Level::LEGENDARY },
-      { type: ResourceType::TYPES[:fish], name: 'Rotting Creek', location: location_data[5][:name], level: Level::LEGENDARY },
-      { type: ResourceType::TYPES[:bush], name: 'Large Mushroom', location: location_data[5][:name], level: Level::LEGENDARY },
-      { type: ResourceType::TYPES[:herd], name: 'Roaming Zombie', location: location_data[5][:name], level: Level::LEGENDARY },
-      { type: ResourceType::TYPES[:ore], name: 'Copper', location: location_data[5][:name], level: Level::LEGENDARY },
-      { type: ResourceType::TYPES[:ore], name: 'Iron', location: location_data[5][:name], level: Level::LEGENDARY }
+      Resource::Crystal.build(name: 'Singing', location_id: 6, level: Level::LEGENDARY),
+      Resource::Crystal.build(name: 'Human-like Bone', location_id: 6, level: Level::LEGENDARY),
+      Resource::Fish.build(name: 'Rotting Creek', location_id: 6, level: Level::LEGENDARY),
+      Resource::Bush.build(name: 'Large Mushroom', location_id: 6, level: Level::LEGENDARY),
+      Resource::Herd.build(name: 'Roaming Zombie', location_id: 6, level: Level::LEGENDARY),
+      Resource::Ore.build(name: 'Copper', location_id: 6, level: Level::LEGENDARY),
+      Resource::Ore.build(name: 'Iron', location_id: 6, level: Level::LEGENDARY)
     ]
   end
 
   def self.location_data
     [
-      {
+      Location.build(
         name: 'Spawnville',
         pos_x: 0,
         pos_y: 0,
         description: 'A small metropolis ruled over by Mayor Parker.',
         bg_color: '#cce6ff'
-      },
-      {
+      ),
+      Location.build(
         name: 'Rolling Hills',
         pos_x: 10,
         pos_y: 21,
         description: 'Gentle hills.  Known for beginner ores and flowers.',
         bg_color: '#8cd98c'
-      },
-      {
+      ),
+      Location.build(
         name: 'Harsh Desert',
         pos_x: 20,
         pos_y: 78,
         description: 'Sun blasted plains.  Odd Cacti grow occasionally.',
         bg_color: '#ffe699'
-      },
-      {
+      ),
+      Location.build(
         name: 'Dark Forest',
         pos_x: -32,
         pos_y: -62,
         description: 'Spooky. Sounds of creatures just out of sight.',
         bg_color: '#003300',
         highlight_color: 'white'
-      },
-      {
+      ),
+      Location.build(
         name: 'Flowing Volcano',
         pos_x: 89,
         pos_y: -62,
         description: 'Tall and steep. It is hard to breathe through the noxious air',
         bg_color: '#cc0000'
-      },
-      {
+      ),
+      Location.build(
         name: 'Alien Ruins',
         pos_x: -93,
         pos_y: 87,
         description: 'There are bones everywhere.',
         bg_color: '#222222',
         highlight_color: 'white'
-      }
+      )
     ]
   end
 end
