215268b02bbf6af86d5857fa8d4809da8cadd1d5
Kent Slaymaker <kentslaymaker@gmail.com>
2025-11-02 17:50:50 -0700

    added specs

--- spec/models/item/craftable/tool/crafting_tool_spec.rb
+++ spec/models/item/craftable/tool/crafting_tool_spec.rb
@@ -3,12 +3,30 @@ require 'rails_helper'
 RSpec.describe Item::Craftable::Tool::CraftingTool do
   let(:user) { create(:user) }
   let(:crafting_tool) { create(:crafting_tool, inventory: user.inventory) }
+  let(:bad_crafting_params) do
+    {
+      item_type: ItemType::CRAFTABLE[:ingot],
+      item_ids: [
+        create(:gatherable_ore, inventory: user.inventory).id,
+        create(:gatherable_fruit, inventory: user.inventory).id,
+        create(:gatherable_ore, inventory: user.inventory).id
+      ]
+    }
+  end
   let(:crafting_params) do
     {
       item_type: ItemType::CRAFTABLE[:ingot],
       item_ids: [
         create(:gatherable_ore, inventory: user.inventory).id,
         create(:gatherable_ore, inventory: user.inventory).id
+      ]
+    }
+  end
+  let(:stacked_crafting_params) do
+    {
+      item_type: ItemType::CRAFTABLE[:ingot],
+      item_ids: [
+        create(:gatherable_ore, inventory: user.inventory, stack_amount: 3).id
       ]
     }
   end
@@ -28,8 +46,21 @@ RSpec.describe Item::Craftable::Tool::CraftingTool do
   end
 
   it 'crafts' do
-    item_count = user.inventory.items.count
+    old_inv = user.inventory.items.count
     crafting_tool.craft(crafting_params)
-    expect(user.inventory.items.count == item_count + 1).to be true
+    expect(user.inventory.items.last).to be_a ItemType::Craftable[:ingot].constantize
+    expect(user.inventory.items.count).to eq(old_inv - 1)
+    crafting_params[:item_ids].each do |id|
+      expect(Item.find_by(id:)).to be_nil
+    end
+  end
+
+  it 'crafts with stacked items' do
+    crafting_tool.craft(stacked_crafting_params)
+    expect(Item.find_by(id: stacked_crafting_params).stack_amount).to eq(1)
+  end
+
+  it 'doesnt craft with bad recipe' do
+    expect { crafting_tool.craft(bad_crafting_params) }.to raise_error CraftyError, message: ErrorMessage::CRAFTING[:failed]
   end
 end
