cd5bcbf93a0c788fbed4338762c4463bd7750385
Kent Slaymaker <kentslaymaker@gmail.com>
2025-11-10 12:39:11 -0700

    refactoer for readability;

--- app/models/item/crafted/tool/crafting_tool.rb
+++ app/models/item/crafted/tool/crafting_tool.rb
@@ -9,15 +9,10 @@ class Item::Crafted::Tool::CraftingTool < Item::Crafted::Tool
     @crafted_item = crafted_item
     @ingredients  = ingredients
 
-    unless equipped_by.spend_energy(energy_usage(crafted_item, ingredients))
-      raise CraftyError, ErrorMessage::USER[:build_additional_pylons]
-    end
-
     can_craft?
 
     craft_the_item
-    crafted = equipped_by.inventory.add_item(@crafted_item)
-    consume_ingredients if crafted
+    consume_ingredients if equipped_by.inventory.add_item(@crafted_item)
 
     @crafted_item
   end
@@ -31,15 +26,28 @@ class Item::Crafted::Tool::CraftingTool < Item::Crafted::Tool
   private
 
   def can_craft?
+    unless equipped_by.spend_energy(energy_usage(@crafted_item, @ingredients))
+      raise CraftyError, ErrorMessage::USER[:build_additional_pylons]
+    end
+
     raise CraftyError, ErrorMessage::INVENTORY[:no_space] if equipped_by.inventory.remaining_space.zero?
 
-    proposed_recipe =
-      @ingredients.each_with_object({}) do |i, hash|
-        hash[i.type] ||= 0
-        hash[i.type]  += i.stack_amount
-      end
+    matches_recipe?
+  end
 
-    matches_recipe?(proposed_recipe)
+  def matches_recipe?
+    match = true
+    @crafted_item.recipe.each do |recipe_type, recipe_amount|
+      match &&= proposed_recipe[recipe_type] >= recipe_amount
+    end
+    raise CraftyError, ErrorMessage::CRAFTING[:wrong_ingredients] unless match
+  end
+
+  def proposed_recipe
+    @proposed_recipe ||= @ingredients.each_with_object({}) do |i, hash|
+      hash[i.type] ||= 0
+      hash[i.type]  += i.stack_amount
+    end
   end
 
   def craft_the_item
@@ -67,14 +75,6 @@ class Item::Crafted::Tool::CraftingTool < Item::Crafted::Tool
 
   def typed_ingredients(type)
     @ingredients.select { |i| i.type == type }
-  end
-
-  def matches_recipe?(proposed_recipe)
-    match = true
-    @crafted_item.recipe.each do |recipe_type, recipe_amount|
-      match &&= proposed_recipe[recipe_type] >= recipe_amount
-    end
-    raise CraftyError, ErrorMessage::CRAFTING[:wrong_ingredients] unless match
   end
 
   def crafted_item_level
