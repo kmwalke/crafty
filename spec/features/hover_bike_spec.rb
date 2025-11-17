cfeae358169999a017deb7a3f55783b3e815b725
Kent Slaymaker <kentslaymaker@gmail.com>
2025-11-17 11:39:37 -0700

    show hoverbike inventory

---
+++ spec/features/hover_bike_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Hover Bike' do
+  let!(:player) { login }
+  let!(:bike) { create(:vehicle_hover_bike, parent_inventory: player.inventory) }
+  let!(:item) { create(:gatherable_fruit, parent_inventory: bike.child_inventory) }
+
+  before do
+    visit game_path
+    click_link bike.name
+    click_link 'Equip'
+  end
+
+  it 'shows the vehicle inventory' do
+    within 'div.inventory' do
+      expect(page).to have_content("#{bike.full_name} Inventory")
+    end
+  end
+
+  it 'shows the items in vehicle inventory' do
+    within 'div.vehicle-inventory' do
+      expect(page).to have_content(item.full_name)
+    end
+  end
+end
