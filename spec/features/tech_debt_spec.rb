0e020bc963cfa387f20ce12314851f114d7e5ea6
Kent Slaymaker <kentslaymaker@gmail.com>
2025-11-20 14:52:58 -0700

    pm

--- spec/features/tech_debt_spec.rb
+++ spec/features/tech_debt_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Tech Debt' do
-  pending 'organize partial views'
   pending 'change user to player'
-  pending 'organize routes'
+
+  pending 'organize partial views AFTER UI'
+  pending 'organize routes AFTER UI'
+  pending 'organize ItemType constants'
+
   pending 'streamline API levels: routes/controller/model'
   pending 'document api levels'
-  pending 'organize ItemType constants'
+
   pending 'run PM script from release.sh'
   # also run from docker startup script
   # add pm.html.erb gitignore
