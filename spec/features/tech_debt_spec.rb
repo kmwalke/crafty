f0fbffad5c89167ae269942bf86453095f361750
Kent Slaymaker <kentslaymaker@gmail.com>
2025-11-20 14:44:06 -0700

    pm

--- spec/features/tech_debt_spec.rb
+++ spec/features/tech_debt_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe 'Tech Debt' do
   pending 'streamline API levels: routes/controller/model'
   pending 'document api levels'
   pending 'organize ItemType constants'
+  pending 'run PM script from release.sh'
+  # also run from docker startup script
+  # add pm.html.erb gitignore
 end
