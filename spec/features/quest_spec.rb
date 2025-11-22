f2de5e648adbbc750fd03106da17cc66a7d44f06
Kent Slaymaker <kentslaymaker@gmail.com>
2025-11-22 12:06:25 -0700

    refine quest spec

--- spec/features/quest_spec.rb
+++ spec/features/quest_spec.rb
@@ -1,12 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Quests' do
+  let!(:player) { create(:user) }
+
+  before do
+    login_as player
+    visit game_path
+
+    within 'div.buildings' do
+      click_link 'Quest Board'
+    end
+  end
+
   describe 'quest board' do
     # quest boards are in towns
     # They list all the quests in an area (user and system generated)
     # Dungeon quests involve an encounter, may be fighting or diplomacy or fetching or something else
     # crafting quests are reverse sales listings
     # Each type of quest should challenge a specific skill/job/set of skills
+
+    it 'shows the quest board' do
+      within '.quest-board-popup' do
+        expect(page).to have_content('Quest Board')
+      end
+    end
+
+    it 'displays the local quests' do
+      within '.quest-board-popup' do
+        expect(page).to have_content(quest.name)
+      end
+    end
   end
 
   describe 'dungeon quests' do
@@ -17,7 +40,7 @@ RSpec.describe 'Quests' do
     end
 
     describe 'delves a solo dungeon' do
-      # player lists a quest in a building/town (quest board?)
+      # player accepts a quest in a building/town (quest board?)
       # player travels to quest location to start the dungeon
       # When you click go on a dungeon, the entire thing is simulated out, 1 room at a time, 1 skill check at a time
       #
@@ -42,6 +65,14 @@ RSpec.describe 'Quests' do
     # Go pickup the cargo from location x and deliver to location y
     # Space trucker quest
     # May be dangers/challenges along the way
+
+    describe 'solo' do
+
+    end
+
+    describe 'group' do
+
+    end
   end
 
   describe 'gathering quests' do
@@ -49,11 +80,27 @@ RSpec.describe 'Quests' do
     # Delve into a dungeon with rare crystals in the back
     # Travel to a far off provence to beg/borrow/steal rare items
     # Indiana Jones is the Experience comp
+
+    describe 'solo' do
+
+    end
+
+    describe 'group' do
+
+    end
   end
 
   describe 'crafting quests' do
     # craft an item and sell it to the lister
     # Travel to the dwarf kingdom to use the fabled Mithril Forge # There you craft the best sword ever made
     # Study under a master biologist to craft the finest bio-engineered pet
+
+    describe 'solo' do
+
+    end
+
+    describe 'group' do
+
+    end
   end
 end
