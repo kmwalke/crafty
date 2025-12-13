f45b8d65d5549bad9fb82fd205c0d392b89960e4
Kent Slaymaker <kentslaymaker@gmail.com>
2025-12-13 10:35:29 -0700

    show dungeon reward

--- app/models/dungeon.rb
+++ app/models/dungeon.rb
@@ -4,13 +4,33 @@ class Dungeon < ApplicationRecord
   has_many :quests
   has_many :rooms, after_add: :calculate_level
 
+  attr_reader :results, :player
+
   def run(player)
-    rooms.map do |room|
-      room.run(player)
-    end
+    init(player)
+    run_rooms
+    gather_reward
+    @results
   end
 
   private
+
+  def init(player)
+    @player  = player
+    @results = {
+      rooms: {}
+    }
+  end
+
+  def run_rooms
+    rooms.each do |room|
+      @results[:rooms][room.name] = room.run(@player)
+    end
+  end
+
+  def gather_reward
+    @results[:reward] = 'You won a nice crafting ingredient.'
+  end
 
   def calculate_level(_room)
     self.level = rooms.pluck(:level).reduce(:+) / rooms.count
--- spec/features/dungeon_spec.rb
+++ spec/features/dungeon_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe 'Quests' do
 
     it 'shows the results' do
       dungeon.rooms.each do |room|
-        expect(page).to have_content("#{room.name} Result:")
+        expect(page).to have_content("#{room.name}")
       end
     end
   end
